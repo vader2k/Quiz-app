8469f4b1ca05259d29c22de4b902395b4ae2b537
danie odukwe <kingdanny295@gmail.com>
2024-01-30 13:47:42 +0100

    adding my questions from an index file

---
+++ src/data.js
@@ -0,0 +1,684 @@
+export const data = [
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:2,
+        question: "Which method is used to change the state in React?",
+        answers: [
+            {
+                options: "this.setState()",
+                correct: true,
+            },
+            {
+                options: "this.stateChange()",
+                correct: false,
+            },
+            {
+                options: "updateState()",
+                correct: false,
+            },
+            {
+                options: "modifyState()",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id:1,
+        question: "What does JSX stand for?",
+        answers: [
+            {
+                options: "JavaScript XML",
+                correct: true,
+            },
+            {
+                options: "JavaScript Extensible",
+                correct: false,
+            },
+            {
+                options: "Java XML",
+                correct: false,
+            },
+            {
+                options: "JavaX",
+                correct: false,
+            },
+        ]
+    },
+]
