55523ba19ba5e21177400844400c9e2fae047143
danie odukwe <kingdanny295@gmail.com>
2024-01-30 14:30:02 +0100

    adding dark and light mode

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,8 +1,14 @@
-
+import { useState, useEffect } from "react"
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa6";
 
 const Quiz = () => {
   return (
-    <div>Quiz</div>
+    <div className="flex flex-col gap-8 p-5">
+      <div className="flex justify-between items-center">
+
+      </div>
+    </div>
   )
 }
 
